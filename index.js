898afbd24c2397bbe5260a9bf528b4f00b84c698
manon jacquin <manon.jacquin@gmail.com>
2018-09-11 16:35:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  const target = document.querySelector('#nested .target');
+  return target
+}
+
+function increaseRankBy(n){
+  // Select all the li in ul.ranked-list 
+    const rankedLis = document.querySelectorAll('ul.ranked-list li');
+  // Increase the ranks by n in rankedLis
+  for (let i = 0; i < rankedLis.length; i++){
+   rankedLis[i].innerHTML = parseInt(rankedLis[i].innerHTML) + n
+
+  }
+}
+
+function deepestChild(){
+  const deepDiv = document.querySelectorAll('div#grand-node div');
+  
+}
